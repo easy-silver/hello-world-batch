13626e5bcb580caeab631bc88905bcd26a3e7d28
이지은/파트너포탈팀 <jieun2.lee@musinsa.com>
2023-02-05 12:52:35 +0900

    Initial commit

---
+++ src/main/java/com/example/helloworldbatch/HelloWorldBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworldbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldBatchApplication.class, args);
+	}
+
+}
